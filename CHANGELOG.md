494347186a298422c0fdda313595a41f8cc724fa
Joakim Kennedy <joakimlkennedy@gmail.com>
2019-01-02 23:28:02 +0000

    Prepare for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Next release
+## 0.6.0
 
 ### Improvements
 
--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version string = "0.6.0-SNAPSHOT"
+const version string = "0.6.0"
 
 var cfgFile string
 
